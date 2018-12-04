bacb8728a0bac66b085802df3a553aef4c5287f1
Yuuki Nishiyama <tetujin@ht.sfc.keio.ac.jp>
2018-12-04 11:32:35 +0200

    Add a number-based DB-type setting function

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -15,16 +15,34 @@ class TestsRealm: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testSettingConfig(){
+        // DatabaseType based DB Type setting (NONE)
+        var config = SensorConfig(["dbType":DatabaseType.NONE]);
+        XCTAssertEqual(config.dbType, DatabaseType.NONE)
+        
+        // DatabaseType based DB Type setting (REALM)
+        config = SensorConfig(["dbType":DatabaseType.REALM]);
+        XCTAssertEqual(config.dbType, DatabaseType.REALM)
+        
+        // Int based DB Type setting (NONE)
+        config.set(config: ["dbType":0])
+        XCTAssertEqual(config.dbType, DatabaseType.NONE)
+        
+        // Int based DB Type setting (REALM)
+        config.set(config: ["dbType":1])
+        XCTAssertEqual(config.dbType, DatabaseType.REALM)
     }
     
     func testSavingFunction(){
@@ -32,32 +50,23 @@ class TestsRealm: XCTestCase {
         let number = 10
         for x in 0..<number {
             let event:AwareObject = AwareObject()
-            
             switch x{
             case 0..<5:
                 event.timestamp = 100
                 break
             default:
                 break
-                
             }
             engine.save([event], "")
-            print(x)
         }
         
-        // let results = engine.fetch(AccelerometerEvent.self, "y==100") as! Results<Object>
         let results = engine.fetch("table", AwareObject.self, nil)
         
         if let castResults = results as? Results<Object> {
-            print("hello")
-            
             XCTAssertEqual(castResults.count, number)
-            
             for result in castResults {
                 print(result)
             }
-        }else{
-            print("error")
         }
     }
 }
