77f519945e3574c1d2da2ba80205064da49ef884
Yuuki Nishiyama <yuukin@iis.u-tokyo.ac.jp>
2025-03-13 06:06:20 +0900

    0.7.7: Testing SwiftPM

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(name: "com.awareframework.ios.sensor.core",
+                      platforms: [.iOS(.v13),],
+                      products: [
+                          .library(name: "com.awareframework.ios.sensor.core", 
+                                   targets: ["com.awareframework.ios.sensor.core"]),
+                      ],
+                    dependencies: [
+                        .package(url: "git@github.com:realm/realm-swift.git", from: "20.0.0"),
+                        .package(url: "git@github.com:SwiftyJSON/SwiftyJSON.git", from: "5.0.2"),
+                    ],  
+                      targets: [.target(name: "com.awareframework.ios.sensor.core",
+                                        dependencies: [
+                                            .product(name: "RealmSwift", package: "realm-swift"),
+                                            .product(name: "SwiftyJSON", package: "SwiftyJSON")
+                                        ],
+                                        path: "com.awareframework.ios.sensor.core/Classes")
+                                ],
+                    swiftLanguageModes: [.v5])
